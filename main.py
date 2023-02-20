da702869c6af0a1de8fc78ea476f33390338070e
Gaurang Bharti <gaurangbharti@gmail.com>
2023-02-20 01:31:58 -0800

    Preset changes

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ API_KEY = str(st.secrets["SIEVE_API_KEY"])
 st.title("Voice Cloner using Tortoise-TTS")
 st.markdown('Built by [Gaurang Bharti](https://twitter.com/gaurang_bharti) powered by [Sieve](https://www.sievedata.com)')
 
-
 st.write("To use, simply upload an audio sample of someone's voice - at least 10-15 seconds long - and enter what you'd like their voice to say in the text box")
-
 
 def send_to_transfersh(file, clipboard=True):
     url = 'https://transfer.sh/'
@@ -58,7 +56,7 @@ def fetch_video(job_id):
     url = data['data'][0]['url']
     return url
 
-def send_data(audio_link, text, name):
+def send_data(audio_link, text, name, preset):
     audio_link = audio_link.split("\n")
     url = "https://mango.sievedata.com/v1/push"
     
@@ -72,7 +70,8 @@ def send_data(audio_link, text, name):
             "audio": {
                 "url": audio_link[0]
                 },
-            "text": text
+            "text": text,
+            "preset": preset
             }
         }
     try:
@@ -88,17 +87,33 @@ def send_data(audio_link, text, name):
 
 audio_in = st.file_uploader("Audio Upload (.wav only)", type='.wav')
 
-text_in = st.text_input('Text input')
+text_in = st.text_input('Text input', max_chars=500)
 
 workflow_name = "custom_tortoise_tts"
 
-if st.checkbox("Noisy input audio? Check this to improve voice quality using FullSubNet (might be slower)"):
-    workflow_name = "custom_fullsubnet_tortoise"
+# if st.checkbox("Noisy input audio? Check this to improve voice quality using FullSubNet (might be slower)"):
+#     workflow_name = "custom_fullsubnet_tortoise"
+
+preset = "ultra_fast"
+
+if st.checkbox("Advanced options (experimental)"):
+    st.code('''         presets = {
+            #recommended, by far the fastest with decent performance
+            'ultra_fast': {'num_autoregressive_samples': 16, 'diffusion_iterations': 30, 'cond_free': False},
+            #the best quality for time, but can take ~10 mins worst case
+            'fast': {'num_autoregressive_samples': 96, 'diffusion_iterations': 80},
+            #basically the best quality but will take much longer than "fast"
+            'standard': {'num_autoregressive_samples': 256, 'diffusion_iterations': 200},
+            #overkill, better than "standard" but not worth the computation
+            'high_quality': {'num_autoregressive_samples': 256, 'diffusion_iterations': 400},
+        }''')
+    preset = st.select_slider(
+        'Select a preset',
+        options=['ultra_fast', 'fast', 'standard', 'high_quality'])
 
 button1 = st.button("Clone Voice")
 
 if st.session_state.get('button') != True:
-
     st.session_state['button'] = button1
 
 if st.session_state['button'] == True:
@@ -108,8 +123,8 @@ if st.session_state['button'] == True:
             f.write(audio_in.getbuffer())
             audio_url = upload_local(f.name)
             f.close()
-        
-    job = send_data(audio_url, text_in, workflow_name)
+
+    job = send_data(audio_url, text_in, workflow_name, preset)
     if job:
         with st.spinner("Processing audio"):
             status = check_status('https://mango.sievedata.com/v1/jobs', 5, str(job))
